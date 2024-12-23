7239b441fdf4882f16f4c72c8b619bea18f12c90
2Palmtrees <2palmtrees@protonmail.com>
2024-12-23 14:30:27 +0100

    clearing

---
+++ README.md
@@ -0,0 +1,4 @@
+# AWS-FILE_BROWSER
+
+- Browse files/folders on your s3 buckets.
+- Upload/delete/rename files/folders on your s3 buckets.
--- frontend/.vite/deps/react-dom.js
+++
@@ -1,5 +0,0 @@
-import {
-  require_react_dom
-} from "./chunk-256Y72TZ.js";
-import "./chunk-GT27OAPC.js";
-export default require_react_dom();
